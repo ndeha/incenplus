e5e02a2a5d6a92467ed44a779935b534a91405b0
ndeha <ninik.dwihastutik@gmail.com>
2019-03-07 22:35:52 +0700

    Delete users_model.php

--- models/users_model.php
+++
@@ -1,8 +0,0 @@
-<?php if(!defined('BASEPATH')) exit('No direct script allowed');
-
-class Users_model extends CI_Model{
-
-	function get_user($q) {
-		return $this->db->get_where('users',$q);
-	}
-}
